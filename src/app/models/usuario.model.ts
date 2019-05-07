2108859c8e3e1823ab459a13338ac4f29f3d2fdc
juan <anarkigotic@gmail.com>
2019-05-07 12:23:58 -0500

    primero coomit  aplicacion sin efectos

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,9 @@
+export class Usuario {
+    constructor(
+        public id: number,
+        public email: string,
+        public fi: string,
+        public last_: string,
+        public avatar: string
+    ) { }
+}
